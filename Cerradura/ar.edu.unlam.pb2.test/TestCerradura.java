4615375f0033c60b0370088c9dda374e8ab54923
braty66 <brian_user@hotmail.com>
2019-10-20 22:37:48 -0300

    cerradura y test correctos.

--- Cerradura/ar.edu.unlam.pb2.test/TestCerradura.java
+++ Cerradura/ar.edu.unlam.pb2.test/TestCerradura.java
@@ -11,10 +11,11 @@ public class TestCerradura {
 		
 	Cerradura cerradura1 = new Cerradura(1234,3);
 	
-	  cerradura1.estaCerrada();
-	 
+	  cerradura1.abrir(1234);
 	  
-	  Assert.assertEquals(true, cerradura1.estaCerrada());
+	 Integer valorEsperado = 1;
+	  
+	  Assert.assertEquals( valorEsperado, cerradura1.contarAperturasExitosas());
 	}
 
 
@@ -26,10 +27,56 @@ public class TestCerradura {
 		
 		Assert.assertEquals(true, cerradura2.estaAbierta());
 	}
-	
+	 
+	 @Test 
+	 public void queCuandoIgreseoClaveIncorrectaNoSeAbra() {
+			
+			Cerradura cerradura2= new Cerradura(2020,3);
+			cerradura2.abrir(2021);
+			
+			Assert.assertEquals(true, cerradura2.estaCerrada());
+		}
+	 
+	 @Test
+	 public void cerrarUnaCerraduraAbierta() {
+			
+			Cerradura cerradura2= new Cerradura(2020,3);
+			cerradura2.abrir(2020);
+			cerradura2.cerrar();
+			
+			Assert.assertEquals(true,cerradura2.estaCerrada());
+		}
+	 
+	 public void laCerraduraEstaBloqueada() {
+		 Cerradura cerradura2= new Cerradura(2020,3);
+			cerradura2.abrir(2021);
+			cerradura2.abrir(2021);
+			cerradura2.abrir(2021);
+			cerradura2.abrir(2021);
+			
+			Assert.assertEquals(true,cerradura2.fueBloqueada());	
+		 
+	 }
+	 public void pruebaSiElMetodoMeDevuleveLasAperturasExitosas() {
+		 Cerradura cerradura2= new Cerradura(2020,3);
+			cerradura2.abrir(2020);
+			cerradura2.abrir(2020);
+			cerradura2.abrir(2020);
+			cerradura2.abrir(2020);
+			Integer valorEsperado=4;
+			Assert.assertEquals(valorEsperado,cerradura2.fueBloqueada());	
+	 }
+	 public void pruebaSiElMetodoMeDevuleveLasAperturasFallidas() {
+		 Cerradura cerradura2= new Cerradura(2020,3);
+			cerradura2.abrir(2020);
+			cerradura2.abrir(2021);
+			cerradura2.abrir(2020);
+			cerradura2.abrir(2020);
+			Integer valorEsperado=1;
+			Assert.assertEquals(valorEsperado,cerradura2.contarAperturasFallidas());	
 
 	
-	
+	 }
 	
 	
 }
--- Cerradura/src/ar/edu/unlam/pb2/parcialUno/Cerradura.java
+++ Cerradura/src/ar/edu/unlam/pb2/parcialUno/Cerradura.java
@@ -1,75 +1,71 @@
 package ar.edu.unlam.pb2.parcialUno;
 
 public class Cerradura {
+
 	private Integer claveDeApertura;
-	private Integer cantidadDeFallosConsecutivosQueLaBloquean;
-	private Boolean estado= false;
-	private int intentosExitosos;
-	private int intentosFallidos;
-    private boolean bloqueoActivado = false;
-	
-	public Cerradura (Integer claveDeApertura, Integer cantidadDeFallosConsecutivosQueLaBloquean) {
-		
+	private Integer cantidadDeFallosConcecutivosQueLaBloquean;
+	private Boolean estado = false;
+	private Integer aperturasFallidas = 0;
+	private Integer aperturasExitosas = 0;
+	public Cerradura(Integer claveDeApertura, Integer cantidadDeFallosConcecutivos) {
+		this.cantidadDeFallosConcecutivosQueLaBloquean = cantidadDeFallosConcecutivos;
 		this.claveDeApertura = claveDeApertura;
-		this.cantidadDeFallosConsecutivosQueLaBloquean = cantidadDeFallosConsecutivosQueLaBloquean;
 		
 	}
-	
-	
-		
-		public void abrir(Integer clave) {
-			if(claveDeApertura.equals(clave) && bloqueoActivado==false) {
-				
-				this.estado=true;
-			}
-			else {
-				intentosFallidos++;
-				
-			}
+	public void abrir (Integer clave ) {
+		if (claveDeApertura.equals(clave) && aperturasFallidas < cantidadDeFallosConcecutivosQueLaBloquean ) {
+			estado =true;
+			aperturasExitosas++;
+		}else {
+			aperturasFallidas++;
 		}
 		
-		
-		
-		
-
-	
-	
-	public void cerrar () {
-		if (estado == true) {
+	}
+	public void cerrar() {
+		if (estado.equals(true)) {
 			estado = false;
+			
+			
 		}
 	}
-
-	public boolean estaAbierta () {
-	            
-		if (estado == true) {
-	            	  return   true;
-	              }
-	              return  false;
-	}
-	public boolean estaCerrada() {
-		if(estado == false) {
+	public Boolean estaAbierta() {
+		if (estado.equals(true)) {
 			return true;
 		}
 		return false;
-	}
-	
-	public boolean fueBloqueada() {
-		if (intentosFallidos >= this.cantidadDeFallosConsecutivosQueLaBloquean) {
-			return true;
-		}
-		return false;
-	}
-	
-	public Integer contarAperturasExitosas() {
 		
-		intentosExitosos++;
-		return intentosExitosos;
+	}
+	public Boolean estaCerrada() {
+		if (estado.equals(false)) {
+			return true;
+		}
+		return false;
+	}
+	public Boolean fueBloqueada() {
+		if (aperturasFallidas >= cantidadDeFallosConcecutivosQueLaBloquean) {
+			return true;
+		}
+		return false;
+		
+	}
+	public Integer contarAperturasExitosas() {
+		return aperturasExitosas;
+	}
+	public Integer contarAperturasFallidas(){
+		return aperturasFallidas;
+	}
+	public Integer getClaveDeApertura() {
+		return claveDeApertura;
+	}
+	public Integer getCantidadDeFallosConcecutivosQueLaBloquean() {
+		return cantidadDeFallosConcecutivosQueLaBloquean;
+	}
+	public Boolean getEstado() {
+		return estado;
 	}
 	
-	public Integer contarAperturasFallidas(){
-		return this.intentosFallidos;
-	}
+	
+	
 }
 
 
